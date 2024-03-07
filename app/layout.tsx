57e50edd1067446556cf76ee6438996009f936a2
Marc Dijoux <marc.dijoux@me.com>
2024-03-07 08:53:13 +0400

    feat: add vercel analytics and speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import '../styles/globals.css';
-
-
- 
-// const roboto = Roboto({
-//     weight: ['100','300','400','500','700','900'],
-//     style: ['normal', 'italic'],
-//     subsets: ['latin'],
-//     display: 'swap',
-// })
 
 export default function RootLayout({
     children,
@@ -20,6 +14,8 @@ export default function RootLayout({
         <html lang="fr">
             <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
                 {children}
+                <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     )
